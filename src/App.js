a11fe7d443f9377eb8c572ac2c0e36bc919077d9
anudeepkovur <70462253+anudeepkovur@users.noreply.github.com>
2024-11-23 13:45:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hi Guys! Welcome to Anudeep Vlogs!!
+          Hi Guys! Welcome to Anudeep Vlogs!!
         </p>
         <a
           className="App-link"
